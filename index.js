955f80816e920515611e02b41d7502667a0cef9d
MD Rajob Ali <119127566+RajobAli@users.noreply.github.com>
2023-05-31 21:17:34 +0600

    racing game

---
+++ index.js
@@ -0,0 +1,81 @@
+var bluecar = document.getElementById("bluecar");
+var racecar = document.getElementById("racecar");
+var result = document.getElementById("result");
+
+var game = document.getElementById("game");
+var buttonleft = document.getElementById("button");
+var buttonRight = document.getElementById("buttons");
+
+
+let displayScore = document.getElementById("score");
+let score = 0;
+
+
+
+
+
+
+function mymessage2(){
+  var raceCarLeft = parseInt(this.window.getComputedStyle(racecar).getPropertyValue("left"));
+  if(raceCarLeft < 260){
+    racecar.style.left = (raceCarLeft + 130) + "px";
+  }
+}
+
+function mymessage1(){
+ var raceCarLeft = parseInt(this.window.getComputedStyle(racecar).getPropertyValue("left"));
+ racecar.style.left = (raceCarLeft - 130) + "px";
+
+}
+
+
+var count = 0;
+
+
+bluecar.addEventListener("animationiteration",function(){
+    var random = ((Math.floor(Math.random() * 3)) * 130)
+    bluecar.style.left = random + "px";
+    count++;
+    
+    displayScore.innerText = `Score:${count}`;   
+})
+
+
+
+
+
+
+window.addEventListener("keydown",function(e){
+    if(e.keyCode == "39"){var raceCarLeft = parseInt(this.window.getComputedStyle(racecar).getPropertyValue("left"))
+    if(raceCarLeft < 260){racecar.style.left = (raceCarLeft + 130) + "px"}};
+
+    if(e.keyCode == "37"){
+        {var raceCarLeft = parseInt(this.window.getComputedStyle(racecar).getPropertyValue("left"))
+    if(raceCarLeft > 0){racecar.style.left = (raceCarLeft - 130) + "px"}};
+
+    }
+})
+
+setInterval(function Gameover (){
+    var bluecarTop = parseInt(window.getComputedStyle(bluecar).getPropertyValue("top"));
+    var bluecarLeft = parseInt(window.getComputedStyle(bluecar).getPropertyValue("left"));
+    var raceCarLeft = parseInt(window.getComputedStyle(racecar).getPropertyValue("left"));
+
+    if((bluecarLeft === raceCarLeft) && (bluecarTop > 250) && (bluecarTop < 450)){
+        result.style.display = "block";
+        game.style.display = "none" ;
+        buttonRight.style.display = "none";
+        buttonleft.style.display = "none";
+        score.innerHTML =  `score : ${counter}`;
+        counter = 0;       
+    }
+},10);
+
+
+
+
+
+
+
+
+
